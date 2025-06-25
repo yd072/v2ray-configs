b5f9b0baaa86662a9e1c01564f273c6b9e6824e6
yd072 <luiquirong@gmail.com>
2025-06-25 17:06:16 +0800

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -78,7 +78,8 @@ def main():
     protocols = ["vmess", "vless", "trojan", "ss", "ssr", "hy2", "tuic", "warp://"]
     links = [
         "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
-        "https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt"
+        "https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt",
+        "https://github.com/Aclashv2rayfree/freev2raynode"
 
     ]
     dir_links = [
